0dcc6ff0d911e7d00c38574c5b576024c9da9194
Yury Podlevskikh <yury.podlevskikh@gmail.com>
2020-04-11 12:04:27 +0200

    Клиент сервиса WebAPI сотрудников

---
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        public EmployeesClient(IConfiguration Configuration) : base(Configuration, WebAPI.Employees)
+        {
+        }
+
+        public IEnumerable<Employee> GetAll() => Get<List<Employee>>(_ServiceAddress);
+
+        public Employee GetById(int id) => Get<Employee>($"{_ServiceAddress}/{id}");
+
+        public void Add(Employee Employee) => Post(_ServiceAddress, Employee);
+
+        public void Edit(int id, Employee Employee) => Put($"{_ServiceAddress}/{id}", Employee);
+
+        public bool Delete(int id) => Delete($"{_ServiceAddress}/{id}").IsSuccessStatusCode;
+
+        public void SaveChanges(){}
+    }
+}
--- Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
+++ Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
 using WebStore.Infrastructure.Interfaces;
 using WebStore.Models;
 
 namespace WebStore.ServiceHosting.Controllers
 {
     //[Route("api/[controller]")]
-    [Route("api/employees")]
+    [Route(WebAPI.Employees)]
     [ApiController]
     public class EmployeesApiController : ControllerBase, IEmployeesData
     {
