c6a70ff5ed65331e5b162ee8911f9d0c66f390d9
Yury Podlevskikh <yury.podlevskikh@gmail.com>
2020-04-12 14:38:03 +0200

    Изменён интерфейс сервиса заказов - данные метода формирования заказа сгруппированы в едуную DTO модель заказа

--- UI/WebStore/Controllers/CartController.cs
+++ UI/WebStore/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.DTO.Orders;
 using WebStore.Domain.Entities.Orders;
 using WebStore.Infrastructure.Interfaces;
 using WebStore.ViewModels;
@@ -55,7 +56,19 @@ namespace WebStore.Controllers
                     OrderViewModel = Model
                 });
 
-            var order = await OrderService.CreateOrderAsync(User.Identity.Name, _CartService.TransformFromCart(), Model);
+            var order_model = new CreateOrderModel
+            {
+                OrderViewModel = Model,
+                OrderItems = _CartService.TransformFromCart().Items
+                .Select(item => new OrderItemDTO
+                {
+                    Id = item.Key.Id,
+                    Price = item.Key.Price,
+                    Quantity = item.Value
+                }).ToList()
+            };
+
+            var order = await OrderService.CreateOrderAsync(User.Identity.Name, order_model);
 
             _CartService.RemoveAll();
 
