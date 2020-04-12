5a84dd97544320beb8d572599e70fec9c5088f37
Yury Podlevskikh <yury.podlevskikh@gmail.com>
2020-04-12 17:24:31 +0200

    Устанавливаем номер версии нашего WebAPI = v1

--- Common/WebStore.Domain/WebAPI.cs
+++ Common/WebStore.Domain/WebAPI.cs
@@ -6,8 +6,8 @@ namespace WebStore.Domain
 {
     public class WebAPI
     {
-        public const string Employees = "api/employees";
-        public const string Products = "api/products";
-        public const string Orders = "api/orders";
+        public const string Employees = "api/v1/employees";
+        public const string Products = "api/v1/products";
+        public const string Orders = "api/v1/orders";
     }
 }
