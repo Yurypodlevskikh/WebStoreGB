0fcbb013ca6e6f3ddb3826e0c5ada6044e3950a2
Yury Podlevskikh <yury.podlevskikh@gmail.com>
2020-04-12 13:40:26 +0200

    Исправляем ошибку связанную с попыткой сериализации объектного графа, содеращего циклы

--- Services/WebStore.Services/Products/InSQL/SqlProductData.cs
+++ Services/WebStore.Services/Products/InSQL/SqlProductData.cs
@@ -17,11 +17,11 @@ namespace WebStore.Infrastructure.Services.InSQL
         public SqlProductData(WebStoreDB db) => _db = db;
 
         public IEnumerable<Section> GetSections() => _db.Sections
-           .Include(section => section.Products)
+           //.Include(section => section.Products)
            .AsEnumerable();
 
         public IEnumerable<Brand> GetBrands() => _db.Brands
-           .Include(brand => brand.Products)
+           //.Include(brand => brand.Products)
            .AsEnumerable();
 
         public IEnumerable<ProductDTO> GetProducts(ProductFilter Filter = null)
