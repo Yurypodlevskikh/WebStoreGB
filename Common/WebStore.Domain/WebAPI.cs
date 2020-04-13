049467fdb7e66df70a3d9d1ecde447f53ca3a82e
Yury Podlevskikh <yury.podlevskikh@gmail.com>
2020-04-13 19:36:32 +0200

    Заготовка контроллеров WebAPI.Identity

--- Common/WebStore.Domain/WebAPI.cs
+++ Common/WebStore.Domain/WebAPI.cs
@@ -9,5 +9,10 @@ namespace WebStore.Domain
         public const string Employees = "api/v1/employees";
         public const string Products = "api/v1/products";
         public const string Orders = "api/v1/orders";
+        public static class Identity
+        {
+            public const string Users = "api/v1/users";
+            public const string Roles = "api/v1/roles";
+        }
     }
 }
