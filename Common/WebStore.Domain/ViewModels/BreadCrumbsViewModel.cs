98dfc58cbbb2aafa33c44b116dd533d3524d3151
Yury Podlevskikh <yury.podlevskikh@gmail.com>
2020-04-27 17:12:40 +0200

    Представление компонента хлебных крошек

---
+++ Common/WebStore.Domain/ViewModels/BreadCrumbsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WebStore.Domain.ViewModels
+{
+    public class BreadCrumbsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public BreadCrumbType BreadCrumbType { get; set; }
+    }
+
+    public enum BreadCrumbType : byte
+    {
+        None,
+        Section,
+        Brand,
+        Product
+    }
+}
--- UI/WebStore/Components/BreadCrumbsViewComponent.cs
+++ UI/WebStore/Components/BreadCrumbsViewComponent.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Components
 {
     public class BreadCrumbsViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
+        private readonly IProductData _ProductData;
+
+        public BreadCrumbsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+
+        public IViewComponentResult Invoke()
+        {
+
+
+            return View();
+        }
     }
 }
